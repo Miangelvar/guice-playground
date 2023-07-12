d2e6fe95705cbc674f3da5a32dd303782cd99193
mvargas <miguel.vargas.bejarano@unillanos.edu.co>
2023-07-12 01:46:43 -0500

    removed unnecesary constructor binding

--- src/main/java/org/example/misc/PersonModule.java
+++ src/main/java/org/example/misc/PersonModule.java
@@ -11,13 +11,9 @@ public class PersonModule extends AbstractModule {
     private static final Faker INSTANCE = Faker.instance(Locale.US);
     @Override
     protected void configure() {
-        try {
             bind(PersonRepository.class).annotatedWith(Names.named("null")).to(NullPersonRepository.class);
             bind(PersonRepository.class).annotatedWith(Names.named("inMemory")).to(InMemoryRepository.class);
-            bind(PersonFinder.class).toConstructor(PersonFinder.class.getConstructor(PersonRepository.class));
+            bind(PersonFinder.class);
             bind(Faker.class).toInstance(INSTANCE);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
